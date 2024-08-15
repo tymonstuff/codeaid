e4a6efada5ff0d601b037589dbc78be6afea61ee
Tymon Rybak <>
2024-08-15 21:09:44 +0100

    update links

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
                       See our Projects
                     </Link>
                     <Link
-                      href="https://github.com/NextJSTemplates/startup-nextjs"
+                      href=""
                       className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                     >
                       Get in Touch
